873d347d0bdd9bfc09c851b07039fc6e005b2655
m-triple-m <triplem656@gmail.com>
2021-11-23 14:37:38 +0530

    ..

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const app_config = {
-  api_url: 'https://musafir-api.herokuapp.com',
+  // api_url: 'https://musafir-api.herokuapp.com',
+  api_url: 'https://travel-api234.herokuapp.com',
 };
